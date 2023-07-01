68baeca32df524c5c14e377026815f583592ba73
Raj-9747 <rajfachara9747@gmail.com>
2023-07-01 21:28:21 +0530

    Assignment-1

---
+++ Assignment_1/Index.js
@@ -0,0 +1,24 @@
+// console.log("Testing..");
+
+
+// "use strict";
+// var a=27;
+// console.log(a);
+
+
+// "use strict";
+// function Test()
+// {
+//     var a=13;
+//     console.log(a);
+// }
+// Test();
+
+function Addition(a,b,b)
+{
+    // "use strict";
+    return a+b+b;
+}
+
+console.log(Addition(10,20));
+
